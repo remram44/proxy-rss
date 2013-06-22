199e450454e70ab48758097d5d7cffb2da27151b
Remi Rampin <remirampin@gmail.com>
2013-06-22 18:59:43 -0400

    Placeholder setup views

---
+++ tweetrss/tweetrss/setup/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import redirect, render
+import tweepy
+
+from tweetrss import settings
+
+
+def index(request):
+    """First page: how to add app to Twitter.
+    """
+    if (settings.CONSUMER_KEY != 'app consumer key' and
+            settings.CONSUMER_SECRET != 'app secret'):
+        if settings.ACCESS_TOKEN != ('user access key',
+                                     'user acces secret'):
+            return redirect('tweetrss.setup.views.getstarted')
+        else:
+            return redirect('tweetrss.setup.views.userkey')
+    return render(
+            request,
+            'setup/index.html')
+
+def appkey(request):
+    """Enter the application key that Twitter generated.
+    """
+
+
+def userkey(request):
+    """Explain how to setup user auth token, choose whether to use callback.
+    """
+
+
+def userkey_callback(request):
+    """If callback is used, user is redirected here after auth.
+    """
+
+
+def userkey_verif(request):
+    """If callback is not used, user enters verification code here.
+    """
+
+
+def getstarted(request):
+    """Once everything is setup, checks configuration and displays help.
+    """
--- tweetrss/tweetrss/urls.py
+++ tweetrss/tweetrss/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('tweetrss',
-    # Examples:
-    #url(r'^$', 'home.views.index', name='index'),
+    # Setup
+    url(r'^$', 'setup.views.index', name='index'),
+    url(r'^appkey$', 'setup.views.appkey', name='appkey'),
+    url(r'^userkey$', 'setup.views.userkey', name='userkey'),
+    #url(r'^userkey_callback$', 'setup.views.userkey_callback', name='userkey_callback'),
+    url(r'^userkey_verif$', 'setup.views.userkey_verif', name='userkey_verif'),
+    url(r'^getstarted$', 'setup.views.getstarted', name='getstarted'),
 
     # Proxy
     url(r'^p/(.+)$', 'proxy.views.timeline', name='timeline'),
 )
 
-handler404 = 'tweetrss.site.error_404'
-handler500 = 'tweetrss.site.error_500'
+handler404 = 'tweetrss.setup.error_404'
+handler500 = 'tweetrss.setup.error_500'
