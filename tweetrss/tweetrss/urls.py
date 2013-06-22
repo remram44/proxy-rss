5f844079dde45cab3cf351fab02c042939f2e9b2
Remi Rampin <remirampin@gmail.com>
2013-06-22 18:57:54 -0400

    Moves proxy urls to top-level urls.py

--- tweetrss/tweetrss/urls.py
+++ tweetrss/tweetrss/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('tweetrss',
     # Examples:
     #url(r'^$', 'home.views.index', name='index'),
-    url(r'^p/', include('tweetrss.proxy.urls')),
+
+    # Proxy
+    url(r'^p/(.+)$', 'proxy.views.timeline', name='timeline'),
 )
 
 handler404 = 'tweetrss.site.error_404'
