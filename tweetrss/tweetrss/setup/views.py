048d4412713165582b367241dd13faa60380df39
Remi Rampin <remirampin@gmail.com>
2013-06-22 21:06:02 -0400

    Implements appkey

--- tweetrss/tweetrss/setup/views.py
+++ tweetrss/tweetrss/setup/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+import os
 import tweepy
 
 from tweetrss import settings
@@ -21,9 +22,78 @@ def index(request):
 def appkey(request):
     """Enter the application key that Twitter generated.
     """
-    return render(
-            request,
-            'setup/appkey.html')
+    missing = set()
+
+    app_key = request.POST.get('fkey')
+    if not app_key:
+        missing.add('fkey')
+    app_secret = request.POST.get('fsecret')
+    if not app_secret:
+        missing.add('fsecret')
+
+    # These parameters are optional
+    user_key = request.POST.get('futoken')
+    user_secret = request.POST.get('fusecret')
+    if user_key or user_secret:
+        if not user_key:
+            missing.add('futoken')
+        if not user_secret:
+            missing.add('fusecret')
+
+    if missing:
+        # Stuff is missing, ask again
+        return render(
+                request,
+                'setup/appkey.html',
+                {
+                    'missing': missing,
+                    'fkey': app_key or '',
+                    'fsecret': app_secret or '',
+                    'futoken': user_key or '',
+                    'fusecret': user_secret or '',
+                })
+    else:
+        # Try to rewrite the config ourselves
+        try:
+            filename = os.path.join(
+                    os.path.dirname(__file__),
+                    '../settings.py')
+            with open(filename, 'r') as fp:
+                lines = list(fp)
+            for lineno in xrange(len(lines)):
+                line = lines[lineno]
+                if line.startswith('CONSUMER_KEY'):
+                    lines[lineno] = 'CONSUMER_KEY = %r\n' % app_key
+                elif line.startswith('CONSUMER_SECRET'):
+                    lines[lineno] = 'CONSUMER_SECRET = %r\n' % app_secret
+                elif user_key:
+                    if line.startswith('ACCESS_TOKEN'):
+                        closeparen = ')' in line
+                        lines[lineno] = (
+                                'ACCESS_TOKEN = (%r,\n'
+                                '                %r)\n' % (
+                                user_key, user_secret))
+                        if not closeparen:
+                            lineno += 1
+                            while ')' not in lines[lineno]:
+                                del lines[lineno]
+                            del lines[lineno]
+            with open(filename, 'w') as fp:
+                for line in lines:
+                    fp.write(line)
+        except IOError:
+            return redirect('tweetrss.setup.views.appkey_manual',
+                    app_key=app_key,
+                    app_secret=app_secret,
+                    user_key=user_key,
+                    user_secret=user_secret)
+        else:
+            # If we set the user auth, we are done
+            if user_key:
+                return redirect('tweetrss.setup.views.getstarted')
+            # Else, take use to the OAuth dance
+            else:
+                return redirect('tweetrss.setup.views.userkey')
 
 
 def userkey(request):
